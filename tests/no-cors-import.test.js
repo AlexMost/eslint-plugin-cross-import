4710b33a716eec83a4496a324c3eb0d07d082bba
a.mostovenko <alexmost1989@gmail.com>
2019-10-28 16:53:37 +0200

    added tests

---
+++ tests/no-cors-import.test.js
@@ -0,0 +1,33 @@
+import { RuleTester } from 'eslint';
+import rule from '../lib/rules/no-cross-import';
+
+const options = [{"scopeRegex":  "\\bpackages\\/(?<namespace>.*?)\\/", "allow": ["shared"]}];
+const parserOptions = { ecmaVersion: 6, sourceType: "module" };
+const ruleTester = new RuleTester();
+const filename = 'packages/test/some/file.js';
+
+ruleTester.run('no-cross-import', rule, {
+    valid: [
+        {
+            filename,
+            code: 'import "jquery"',
+            options,
+            parserOptions,
+        },
+        {
+            filename,
+            code: 'import "packages/shared/file.js"',
+            options,
+            parserOptions,
+        }
+    ],
+    invalid: [
+        {
+            code: 'import { Some } from "packages/other/file.js"',
+            filename,
+            options,
+            parserOptions,
+            errors: [{ message: `You can't make cross import "other" in "test"` }]
+        }
+    ]
+})
